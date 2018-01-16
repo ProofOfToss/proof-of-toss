5b7e0e1b113861c18c3fd88040e1f0f91709d605
Nickolay Savchenko <n.savchenko@axioma.lv>
2018-01-16 15:16:12 +0300

    35004 add gitlab ci config. fixed prod webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -137,10 +137,6 @@ module.exports = {
           presets: ['react']
         }
       },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss', 'sass')
-      },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
@@ -157,6 +153,11 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+      },
+      {
+        test: /\.scss$/,
+        // include: paths.appSrc,
+        loader: 'style!css?importLoaders=1!sass?sourceMap!postcss'
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
