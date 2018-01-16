834e5c8e0cd48b398f833be14f20681b836acb90
Aleksandr Borodulin <a.borodulin@axioma.lv>
2018-01-16 13:01:27 +0300

    feature/34960_site_structure / 3
    Fix for sticky footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router'
 import NavbarUser from './components/navbar_user/NavbarUser'
 import Language from './components/navbar_user/Language'
@@ -26,75 +26,77 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            { /* Brand and toggle get grouped for better mobile display */ }
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar" />
-                <span className="icon-bar" />
-                <span className="icon-bar" />
-              </button>
-              <Link to="/" className="navbar-brand">Proof of toss</Link>
-            </div>
+      <Fragment>
+        <div className="App">
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              { /* Brand and toggle get grouped for better mobile display */ }
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                </button>
+                <Link to="/" className="navbar-brand">Proof of toss</Link>
+              </div>
 
-            { /* Collect the nav links, forms, and other content for toggling */ }
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul className="nav navbar-nav">
+              { /* Collect the nav links, forms, and other content for toggling */ }
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
 
-                {/*<li className={ this._menuLinkClass('/storage') }><Link to="/storage" className="pure-menu-link">StorageSC test</Link></li>*/}
-                {/*<li className={ this._menuLinkClass('/events') }><Link to="/events" className="pure-menu-link">Events list</Link></li>*/}
-                {/*<li className={ this._menuLinkClass('/new_event') }><Link to="/new_event" className="pure-menu-link">New event</Link></li>*/}
+                  {/*<li className={ this._menuLinkClass('/storage') }><Link to="/storage" className="pure-menu-link">StorageSC test</Link></li>*/}
+                  {/*<li className={ this._menuLinkClass('/events') }><Link to="/events" className="pure-menu-link">Events list</Link></li>*/}
+                  {/*<li className={ this._menuLinkClass('/new_event') }><Link to="/new_event" className="pure-menu-link">New event</Link></li>*/}
 
-                <li className={ this._menuLinkClass('/new_event') }><Link to="/new_event" className="pure-menu-link">New event</Link></li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown">Wallet <span className="caret"></span></a>
-                  <ul className="dropdown-menu" role="menu">
-                    <li><Link to="/wallet" className="pure-menu-link">Wallet</Link></li>
-                    <li><Link to="/wallet/deposit" className="pure-menu-link">Deposit</Link></li>
-                    <li><Link to="/wallet/send" className="pure-menu-link">Send</Link></li>
-                  </ul>
-                </li>
+                  <li className={ this._menuLinkClass('/new_event') }><Link to="/new_event" className="pure-menu-link">New event</Link></li>
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown">Wallet <span className="caret"></span></a>
+                    <ul className="dropdown-menu" role="menu">
+                      <li><Link to="/wallet" className="pure-menu-link">Wallet</Link></li>
+                      <li><Link to="/wallet/deposit" className="pure-menu-link">Deposit</Link></li>
+                      <li><Link to="/wallet/send" className="pure-menu-link">Send</Link></li>
+                    </ul>
+                  </li>
 
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown">Play <span className="caret"></span></a>
-                  <ul className="dropdown-menu" role="menu">
-                    <li><Link to="/play" className="pure-menu-link">Play</Link></li>
-                    <li><Link to="/play/event" className="pure-menu-link">Event</Link></li>
-                    <li><Link to="/play/event/bid_confirmation" className="pure-menu-link">Bid confirmation</Link></li>
-                    <li><Link to="/play/event/contest_result" className="pure-menu-link">Contest result</Link></li>
-                  </ul>
-                </li>
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown">Play <span className="caret"></span></a>
+                    <ul className="dropdown-menu" role="menu">
+                      <li><Link to="/play" className="pure-menu-link">Play</Link></li>
+                      <li><Link to="/play/event" className="pure-menu-link">Event</Link></li>
+                      <li><Link to="/play/event/bid_confirmation" className="pure-menu-link">Bid confirmation</Link></li>
+                      <li><Link to="/play/event/contest_result" className="pure-menu-link">Contest result</Link></li>
+                    </ul>
+                  </li>
 
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown">Payments <span className="caret"></span></a>
-                  <ul className="dropdown-menu" role="menu">
-                    <li><Link to="/payments" className="pure-menu-link">Payments</Link></li>
-                    <li><Link to="/payments/withdraw" className="pure-menu-link">Withdraw payment</Link></li>
-                  </ul>
-                </li>
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown">Payments <span className="caret"></span></a>
+                    <ul className="dropdown-menu" role="menu">
+                      <li><Link to="/payments" className="pure-menu-link">Payments</Link></li>
+                      <li><Link to="/payments/withdraw" className="pure-menu-link">Withdraw payment</Link></li>
+                    </ul>
+                  </li>
 
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown">Judge <span className="caret"></span></a>
-                  <ul className="dropdown-menu" role="menu">
-                    <li><Link to="/judge" className="pure-menu-link">Judge</Link></li>
-                    <li><Link to="/judge/confirm_result" className="pure-menu-link">Confirm result</Link></li>
-                  </ul>
-                </li>
-              </ul>
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown">Judge <span className="caret"></span></a>
+                    <ul className="dropdown-menu" role="menu">
+                      <li><Link to="/judge" className="pure-menu-link">Judge</Link></li>
+                      <li><Link to="/judge/confirm_result" className="pure-menu-link">Confirm result</Link></li>
+                    </ul>
+                  </li>
+                </ul>
 
-              <ul className="nav navbar-nav navbar-right">
-                <NavbarUser />
-                <Language />
-              </ul>
+                <ul className="nav navbar-nav navbar-right">
+                  <NavbarUser />
+                  <Language />
+                </ul>
 
-            </div>{ /* /.navbar-collapse */ }
-          </div>{ /* /.container-fluid */ }
-        </nav>
+              </div>{ /* /.navbar-collapse */ }
+            </div>{ /* /.container-fluid */ }
+          </nav>
 
-        {this.props.children}
+          {this.props.children}
+        </div>
 
         <footer>
           <div className="container">
@@ -144,7 +146,7 @@ class App extends Component {
             </div>
           </div>
         </footer>
-      </div>
+      </Fragment>
     );
   }
 }
