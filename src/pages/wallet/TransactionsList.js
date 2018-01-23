d6a31049f8908efd81bb52c18d6b122e63e13b2b
Aleksandr Borodulin <a.borodulin@axioma.lv>
2018-01-23 11:54:20 +0300

    feature/34964_logic_wallet / 2

--- src/pages/wallet/TransactionsList.js
+++ src/pages/wallet/TransactionsList.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import ReactPaginate from 'react-paginate';
 import { browserHistory } from 'react-router'
+import { connect } from 'react-redux';
+import { getMyTransactions } from './../../util/token'
 import TransactionItem from './TransactionItem'
 
 class TransactionList extends Component {
@@ -11,26 +13,22 @@ class TransactionList extends Component {
     this.handlePageClick = this.handlePageClick.bind(this)
 
     this.state = {
+      perPage: 5,
       pageCount: 10,
       transactions: [
-        {id: 1, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'aKjmHRXCHg', sum: 0.21, fee: 0.0001},
-        {id: 2, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'CkYKXUpNx1', sum: 1.54, fee: 0.0017},
-        {id: 3, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'hYp2PcijCH', sum: 6.76, fee: 0.0023},
-        {id: 4, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'hYp2PcijCH', sum: 0.34, fee: 0.0003},
-        {id: 5, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'aKjmHRXCHg', sum: 0.21, fee: 0.0001},
-        {id: 6, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'CkYKXUpNx1', sum: 1.54, fee: 0.0017},
-        {id: 7, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'hYp2PcijCH', sum: 6.76, fee: 0.0023},
-        {id: 8, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'hYp2PcijCH', sum: 0.34, fee: 0.0003}
+        // {id: 1, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'aKjmHRXCHg', sum: 0.21, fee: 0.0001},
+        // {id: 2, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'CkYKXUpNx1', sum: 1.54, fee: 0.0017},
+        // {id: 3, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'hYp2PcijCH', sum: 6.76, fee: 0.0023},
+        // {id: 4, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'hYp2PcijCH', sum: 0.34, fee: 0.0003},
+        // {id: 5, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'aKjmHRXCHg', sum: 0.21, fee: 0.0001},
+        // {id: 6, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'CkYKXUpNx1', sum: 1.54, fee: 0.0017},
+        // {id: 7, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'out', walletNumber: 'hYp2PcijCH', sum: 6.76, fee: 0.0023},
+        // {id: 8, time: this.randomDate(new Date(2012, 0, 1), new Date()), type: 'in', walletNumber: 'hYp2PcijCH', sum: 0.34, fee: 0.0003}
       ]
     }
   }
 
-  randomDate(start, end) {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-  }
-
   handlePageClick(data) {
-    console.log(data)
     browserHistory.push(`/wallet/${data.selected+1}`);
   }
 
@@ -38,45 +36,69 @@ class TransactionList extends Component {
     return `wallet/${page}`
   }
 
+  componentWillMount() {
+    const transactions = getMyTransactions(this.props.web3);
+    this.setState({
+      pageCount: transactions.length / this.state.perPage,
+      allTransactions: transactions
+    })
+  }
+
   render() {
     const page = parseInt(this.props.page, 10) || 1;
+    const hasTransactions = this.state.transactions.length > 0;
+    let content;
+
+    if(hasTransactions) {
+      content =
+        <Fragment>
+          <table className="table">
+            <thead>
+            <tr>
+              <th>Time</th>
+              <th>Type</th>
+              <th>Wallet number</th>
+              <th>Sum</th>
+              <th>Fee</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.transactions.map(function(listItem, key){
+              return <TransactionItem  key={key} item={listItem} />
+            })}
+            </tbody>
+          </table>
+
+          <ReactPaginate previousLabel={"previous"}
+                         nextLabel={"next"}
+                         breakLabel={<a href="">...</a>}
+                         breakClassName={"break-me"}
+                         pageCount={this.state.pageCount}
+                         initialPage={page}
+                         marginPagesDisplayed={2}
+                         pageRangeDisplayed={5}
+                         onPageChange={this.handlePageClick}
+                         hrefBuilder={this.hrefBuilder}
+                         containerClassName={"pagination"}
+                         subContainerClassName={"pages pagination"}
+                         activeClassName={"active"} />
+        </Fragment>
+    } else {
+        content = <div className="well">No transactions</div>
+    }
 
     return(
       <Fragment>
-        <table className="table">
-          <thead>
-          <tr>
-            <th>Time</th>
-            <th>Type</th>
-            <th>Wallet number</th>
-            <th>Sum</th>
-            <th>Fee</th>
-          </tr>
-          </thead>
-          <tbody>
-          {this.state.transactions.map(function(listItem){
-            return <TransactionItem  key={listItem.id} item={listItem} />
-          })}
-          </tbody>
-        </table>
-
-        <ReactPaginate previousLabel={"previous"}
-                       nextLabel={"next"}
-                       breakLabel={<a href="">...</a>}
-                       breakClassName={"break-me"}
-                       pageCount={this.state.pageCount}
-                       initialPage={page}
-                       marginPagesDisplayed={2}
-                       pageRangeDisplayed={5}
-                       onPageChange={this.handlePageClick}
-                       hrefBuilder={this.hrefBuilder}
-                       containerClassName={"pagination"}
-                       subContainerClassName={"pages pagination"}
-                       activeClassName={"active"} />
+        {content}
       </Fragment>
-
     )
   }
 }
 
-export default TransactionList
+function mapPropsToState(state) {
+  return {
+    web3: state.web3.web3
+  };
+}
+
+export default connect(mapPropsToState)(TransactionList);
--- src/util/token.js
+++ src/util/token.js
@@ -15,8 +15,28 @@ function getMyBalance(web3) {
     });
 }
 
+//TODO: get contract fromBlock, walletNumber, fee
 function getMyTransactions(web3) {
+  const contract = require('truffle-contract');
+  const token = contract(TokenContract);
 
+  token.setProvider(web3.currentProvider);
+
+  return token.deployed()
+    .then((instance) => {
+      let items = [];
+      const address = web3.eth.accounts[0];
+      instance.Transfer({}, { fromBlock: 0, toBlock: 'pending' }, (err, res) => {
+        items.push({
+          time: web3.eth.getBlock(res.blockNumber).timestamp,
+          type: res.args.to === address ? 'in' : 'out',
+          walletNumber: 'aKjmHRXCHg',
+          sum: web3.fromWei(res.args.value).toNumber(),
+          fee: 0.0001
+        })
+      });
+      return items;
+    });
 }
 
 export { getMyBalance, getMyTransactions };
