2f592db4490035d428b11b49047448efed477283
Nickolay Savchenko <n.savchenko@axioma.lv>
2018-05-15 12:37:57 +0300

    35957 use avg price in gasPriceOracle

--- src/util/gasPriceOracle.js
+++ src/util/gasPriceOracle.js
@@ -136,9 +136,9 @@ function getGasCalculation(web3, gasAmount) {
 
         resolve({
           gasLimit: gasLimit,
-          gasPrice: Math.round(gasPrices.max),
+          gasPrice: Math.round(gasPrices.avg),
           minFee: web3.fromWei((gasLimit * gasPrices.min), 'ether'),
-          fee: web3.fromWei((gasLimit * gasPrices.max), 'ether'),
+          fee: web3.fromWei((gasLimit * gasPrices.avg), 'ether'),
           gasPriceStr: Math.round(web3.fromWei(gasPrices.avg, 'gwei')).toFixed(config.view.gwei_precision) + ' gwei'
         });
       });
