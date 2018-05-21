1c66bc21c483c32116a4ade4000853943535621c
Aleksandr Borodulin <a.borodulin@axioma.lv>
2018-05-21 11:05:53 +0300

    feature/36261_tags_field

--- src/components/new_event/TagsField.js
+++ src/components/new_event/TagsField.js
@@ -68,6 +68,8 @@ class TagsField extends Component {
                  renderInput={this.renderInput}
                  onlyUnique={true}
                  maxTags="10"
+                 addKeys={[9, 13, 32]}
+                 addOnBlur={true}
                  className='react-tagsinput form-control' />
       { this._showErrors() &&
         <span id="helpBlock" className="help-block">{ this.props.translate('pages.new_event.form.tags.error') }</span>
